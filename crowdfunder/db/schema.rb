43f49f154722cce231e7cb746fb7913dcc000f5c
Daiani Andrade <daiandrade2011@gmail.com>
2015-05-13 16:45:40 -0400

    pledge model has been created

--- crowdfunder/db/schema.rb
+++ crowdfunder/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512195045) do
+ActiveRecord::Schema.define(version: 20150513204232) do
+
+  create_table "pledges", force: :cascade do |t|
+    t.integer  "reward_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
