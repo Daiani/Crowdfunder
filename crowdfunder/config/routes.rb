df94f0b4363b15cb867f294210dfad908ddaa9ae
Daiani Andrade <daiandrade2011@gmail.com>
2015-05-13 16:53:11 -0400

    done

--- crowdfunder/config/routes.rb
+++ crowdfunder/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'projects#index'
   resources :projects, only: [:index, :new, :create, :show]
+  re
   
 
   # The priority is based upon order of creation: first created -> highest priority.
