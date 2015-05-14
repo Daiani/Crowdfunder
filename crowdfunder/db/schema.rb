8e7489ded023e95fe8291b97d872eba809495ee3
Daiani Andrade <daiandrade2011@gmail.com>
2015-05-13 22:35:23 -0400

    done

--- crowdfunder/db/schema.rb
+++ crowdfunder/db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 20150513204232) do
 
   create_table "pledges", force: :cascade do |t|
@@ -22,7 +19,6 @@ ActiveRecord::Schema.define(version: 20150513204232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
