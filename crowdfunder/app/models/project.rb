f70da82c6caa1d299fdfa97df6176a535170ddb7
Patricia Kore <patricia.kore@yahoo.com>
2015-05-14 15:52:43 -0400

    pledges associations

--- crowdfunder/app/models/project.rb
+++ crowdfunder/app/models/project.rb
@@ -1,11 +1,7 @@
 class Project < ActiveRecord::Base
 	has_many :rewards
+  has_many :pledges, through: :rewards
+  belongs_to :user
 	accepts_nested_attributes_for :rewards 
 	# :reject_if :all_blank, allow_destroy: true
 end
-
-
-class Reward < ActiveRecord::Base
-	belongs_to :Project
-end
-
