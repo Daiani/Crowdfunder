2f841ad0ddf93aff12ab020895fbc5114cbf9b5c
Daiani Andrade <daiandrade2011@gmail.com>
2015-05-15 15:45:06 -0400

    fixing show project errors

--- crowdfunder/app/controllers/projects_controller.rb
+++ crowdfunder/app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-before_filter :require_login, except: [:index]
+before_filter :require_login, except: [:index, :show]
+
 	def index
 		@projects = Project.all
 	end
@@ -18,16 +19,34 @@ def create
  @project = Project.new(project_params)
 
   if @project.save
-  	redirect_to projects_path
+  	redirect_to projects_url
   else
      render 'new'	
 	end
+
+def show
+  @project = Project.find(params[:id])
+end
+
+def destroy
+  @project = Project.find(params[:id])
+  if @project.pledges.blank? == false
+    flash[:alert] = "Project can't be deleted."
+  else
+  @project.destroy  
+  flash[:notice] = "Project #{project.name} has been deleted."
+end
+  redirect_to projects_url
+
+  end
+
+
 end
 
 
 private
 def project_params
-	params.require(:project).permit(:name, :description, :funding_goals, :start_date, :finish_date , rewards_attributes: [:amount, :description, :_destroy])
+	params.require(:project).permit(:name, :description, :funding_goals, :start_date, :end_date , rewards_attributes: [:amount, :description, :_destroy])
 end
 
 end
